21fa444a29d1b13607a9f652a9c2c0e23c86611a
Steven Degutis <sbdegutis@gmail.com>
2017-11-24 00:13:58 -0600

    Fixed some typos in TypeScript file.

--- Autumn/types.d.ts
+++ Autumn/types.d.ts
@@ -49,102 +49,102 @@ declare global {
          * Returns an app object for the given pid.
          * @param pid process id of the app
          */
-        static appForPid(pid: number): App { return null; }
+        static appForPid(pid: number): App;
         
         /**
          * Returns any running apps that have the given bundleID.
          * @param id Bundle ID, e.g. "com.apple.TextEdit"
          */
-        static appsForBundleID(id: string): [App] { return null; }
+        static appsForBundleID(id: string): App[];
         
         /**
          * Returns all currently running apps.
          */
-        static runningApps(): [App] { return null; }
+        static runningApps(): App[];
         
         /**
          * Launches the app with the given name, or activates it if it's already running.
          * Returns true if it launched or was already launched; otherwise false (presumably only if the app doesn't exist).
          * @param name Localized name of the app.
          */
-        static open(name: string): boolean { return null; }
+        static open(name: string): boolean;
         
         /**
          * Checks whether this app object is the same as another.
          * @param app Another app object.
          */
-        isEqual(app: App): boolean { return null; }
+        isEqual(app: App): boolean;
         
         /**
          * Returns the main window of the given app, or nil.
          */
-        mainWindow(): Win { return null; }
+        mainWindow(): Win;
         
         /**
          * Returns all open windows owned by the given app.
          */
-        allWindows(): [Win] { return null; }
+        allWindows(): Win[];
         
         /**
          * Returns only the app's windows that are visible.
          */
-        visibleWindows(): [Win] { return null; }
+        visibleWindows(): Win[];
         
         /**
          * True if the app has the spinny circle of death.
          */
-        isUnresponsive(): boolean { return null; }
+        isUnresponsive(): boolean;
         
         /**
          * Returns the localized name of the app (in UTF8).
          */
-        title(): string { return null; }
+        title(): string;
         
         /**
          * Returns the bundle identifier of the app.
          */
-        bundleID(): string { return null; }
+        bundleID(): string;
         
         /**
          * Unhides the app (and all its windows) if it's hidden.
          */
-        unhide(): boolean { return null; }
+        unhide(): boolean;
         
         /**
          * Hides the app (and all its windows).
          */
-        hide(): boolean { return null; }
+        hide(): boolean;
         
         /**
          * Tries to terminate the app.
          */
-        kill() { return null; }
+        kill(): void;
         
         /**
          * Definitely terminates the app.
          */
-        forceKill() { return null; }
+        forceKill(): void;
         
         /**
          * Returns whether the app is currently hidden.
          */
-        isHidden(): boolean { return null; }
+        isHidden(): boolean;
         
         /**
          * Returns the app's process identifier.
          */
-        pid(): number { return null; }
+        pid(): number;
         
         /**
          * Returns the string 'dock' if the app is in the dock, 'no-dock' if not, and 'no-gui' if it can't even have GUI elements if it wanted to.
          */
-        kind(): string { return null; }
+        kind(): string;
         
         /**
          * Tries to activate the app (make its key window focused) and returns whether it succeeded.
          * @param allWindows Whether all windows are brought to the front.
          */
-        activate(allWindows: boolean): boolean { return null; }
+        activate(allWindows: boolean): boolean;
         
     }
     
@@ -166,12 +166,12 @@ declare global {
         /**
          * Returns the window that currently has focus.
          */
-        static focusedWindow(): Win { return null; }
+        static focusedWindow(): Win;
         
         /**
          * Returns handles to every window currently open in no specific order.
          */
-        static allWindows(): [Win] { return null; }
+        static allWindows(): Win[];
         
     }
     
